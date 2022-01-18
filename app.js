5299de25ddefef823d40d718d4b8a95cce5429d2
Jeanet Lin <linett@gmail.com>
2022-01-18 14:49:45 +0100

    iteration 3.1

--- app.js
+++ app.js
@@ -14,12 +14,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
+
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+//iteration 3.1 
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    //console.log('Beers from the database: ', beersFromApi)
+    console.log(beersFromApi)
+    res.render('beers', {beersFromApi});
+  })
+  .catch(error => console.log(error));
+
+  
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
